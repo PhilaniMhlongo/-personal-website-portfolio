63addc7673c0711b3e6a2c8a8fa2c8fbf66cc2dd
Philani Mhlongo <61078948+PhilaniMhlongo@users.noreply.github.com>
2024-07-01 05:00:50 +0200

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,21 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Philani",
   middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  lastName: "Mhlongo",
+  message: " Technology is best when it brings people together ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/PhilaniMhlongo",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+    
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/philani-mhlongo-720382131/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+   
   ],
 };
 
@@ -51,11 +41,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("https://raw.githubusercontent.com/PhilaniMhlongo/PhilaniMhlongo.github.io/main/img/Philani.jpeg"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "I am a highly motivated and detail-oriented individual with a strong work ethic. My passion for coding, particularly in Python and Java, drives me to continually seek opportunities to expand my knowledge and expertise. Thriving in fast-paced environments, I embrace challenges that push my boundaries and foster both personal and professional growth. My dedication to excellence and commitment to continuous learning enable me to excel in dynamic settings and make meaningful contributions to any team or project I am a part of.",
+  resume: "https://drive.google.com/file/d/1NrHU73Zz4kjQ2C9W4w29WERzGs966n-X/view?usp=sharing",
 };
 
 // PROJECTS SECTION
@@ -66,7 +56,7 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  gitHubUsername: "PhilaniMhlongo", //i.e."johnDoe12Gh"
   reposLength: 4,
   specificRepos: [],
 };
@@ -104,10 +94,10 @@ const skills = {
     { name: "SQL", value: 75 },
     { name: "Data Structures", value: 85 },
     { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
+    { name: "Java", value: 90 },
     { name: "React", value: 65 },
     { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    
   ],
   softSkills: [
     { name: "Goal-Oriented", value: 80 },
@@ -126,8 +116,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for full-time Software Engineering opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "mhlongophilani04@gmail.com",
 };
 
 const experiences = {
